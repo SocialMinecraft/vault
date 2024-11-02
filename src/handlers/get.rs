a576030f6a9c4ebb6259a3b7873be318ff50f0e4
Dylan Garvis <dylan@garvis.dev>
2024-11-02 13:27:11 -0400

    feat: List items in vault.

--- src/handlers/get.rs
+++ src/handlers/get.rs
@@ -4,47 +4,46 @@ use crate::proto::vault::{Vault, VaultSlot};
 use crate::proto::vault_get::{GetVault, GetVaultResponse};
 use crate::store::Store;
 use protobuf::{Message};
+use crate::proto::vault_store::StoreVaultItemResponse;
 
 pub async fn get(db: Store, nc: Client, msg: async_nats::Message) -> anyhow::Result<()> {
     let request = GetVault::parse_from_bytes(&msg.payload).unwrap();
-    // todo - trace logging
 
     if let Some(reply) = msg.reply {
 
-        /*let name = &request.subject[6..];
-        client
-            .publish(reply, format!("hello, {}", name).into())
-            .await?;*/
+        // get vault size
+        let size = match db.vault_size(&request.uuid).await {
+            Ok(size) => size,
+            Err(e) => {
+                return Err(e);
+            }
+        };
 
+        // Get each vault item
         let mut vault = Vault::new();
-        for n in 0..2 {
+        for n in 0..size {
             let mut slot = VaultSlot::new();
             slot.is_locked = false;
             slot.cooldown_seconds = 0;
-            vault.slots.push(slot);
-        }
-        for n in 2..5 {
-            let mut slot = VaultSlot::new();
-            slot.is_locked = false;
-            slot.cooldown_seconds = 500;
-            vault.slots.push(slot);
-        }
-        for n in 5..8 {
-            let mut slot = VaultSlot::new();
-            slot.is_locked = true;
-            slot.cooldown_seconds = 0;
+
+            let item = match db.get_item(&request.uuid, n).await {
+                Ok(item) => item,
+                Err(e) => {
+                    return Err(e);
+                }
+            };
+
+            if item.is_some() {
+                slot.item = MessageField::some(item.unwrap());
+            }
             vault.slots.push(slot);
         }
 
+        // build response and send
         let mut resp = GetVaultResponse::new();
         resp.vault = MessageField::some(vault);
-
-        // Serialize to bytes
         let encoded: Vec<u8> = resp.write_to_bytes().unwrap();
-
-        futures::executor::block_on(
-            nc.publish(reply, encoded.into())
-        ).expect("Could not send reply to get vault.");
+        nc.publish(reply, encoded.into()).await?;
     }
 
     return Ok(());
